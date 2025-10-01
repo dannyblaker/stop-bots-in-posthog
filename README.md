c341bc0af0c527b4539515a24db7abd904996141
Danny Blaker <noemail@nothing.com>
2025-10-02 00:23:32 +1000

    init

---
+++ README.md
@@ -0,0 +1,33 @@
+# Remove Bots from PostHog Tracking
+
+This JavaScript snippet helps prevent bot traffic from polluting your [PostHog](https://posthog.com) analytics.  
+
+It detects common web crawlers, scrapers, and bots based on the browser’s `User-Agent` string and automatically opts them out of tracking using the PostHog API's `opt_out_capturing()` method.
+
+**You are most welcome to use this code in your commercial projects, all that I ask in return is that you credit my work by providing a link back to this repository. Thank you & Enjoy!**
+
+
+## How It Works
+
+- The script waits until the global `posthog` object is available.
+- It checks the browser’s `navigator.userAgent`.
+- If the `User-Agent` matches a large list of known bots and crawlers:
+  - A PostHog event (`labelling user as a bot`) is captured with a flag (`is_bot: true`).
+  - The bot is then opted out of all further tracking (`posthog.opt_out_capturing()`).
+- If the visitor is a normal user, tracking continues as usual.
+
+This ensures your analytics data is cleaner and more representative of real human behavior. 
+
+
+## Usage
+
+1. Copy the `remove_bots_posthog.js` file into your project.
+2. Include it **after** loading PostHog in your frontend bundle or HTML page:
+    ```html
+    <script src="https://your-cdn/remove_bots_posthog.js"></script>
+    ```
+
+    or if bundling:
+    ```
+    import './remove_bots_posthog.js';
+    ```
---
+++ remove_bots_posthog.js
@@ -0,0 +1,29 @@
+(async () => {
+  while (!window.hasOwnProperty("posthog"))
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+  let u_agent = navigator.userAgent;
+
+  if (u_agent != null && u_agent.length > 0) {
+
+    var botPattern = "(googlebot\/|bot|Googlebot-Mobile|Googlebot-Image|Google favicon|Mediapartners-Google|bingbot|slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon |httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis)";
+    var re = new RegExp(botPattern, 'i');
+
+    if (re.test(u_agent)) {
+
+      if (posthog.has_opted_out_capturing()) {
+        console.log("bot opted out already");
+      } else {
+        posthog.capture(
+          'labelling user as a bot',
+          {
+            $set_once: { is_bot: true },
+          }
+        );
+        console.log("opting out bot");
+
+        posthog.opt_out_capturing();
+      }
+    }
+  }
+})();
